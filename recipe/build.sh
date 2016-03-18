1ed8180019390de9b26ab3e06640fc23fe8d60ae
ocefpaf <ocefpaf@gmail.com>
2016-03-18 14:20:14 -0300

    Compile nco with ESMF

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
+export HAVE_ANTLR=yes
 export HAVE_NETCDF4_H=yes
 export NETCDF_ROOT=$PREFIX
 
 if [[ $(uname) == Darwin ]]; then
-export LDFLAGS="-headerpad_max_install_names"
-    ./configure \
-        HAVE_ANTLR=yes \
-        --prefix=$PREFIX \
-        --disable-regex \
-        --disable-shared \
-        --disable-doc
+  export LDFLAGS="-headerpad_max_install_names"
+  ARGS="--disable-regex --disable-shared --disable-doc"
 else
-    ./configure \
-        HAVE_ANTLR=yes \
-        --prefix=$PREFIX \
-        --disable-dependency-tracking \
-        --enable-netcdf4 \
-        --disable-static \
-        --disable-udunits \
-        --enable-udunits2
+  ARGS="--disable-dependency-tracking"
 fi
 
+./configure --prefix=$PREFIX --enable-esmf $ARGS
+
 make
+
+if [[ $(uname) != Darwin ]]; then
+make check
+fi
+
 make install
